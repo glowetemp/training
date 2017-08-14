22146963393d888dd73471ef1c9fc35b8683121e
HashiCorp Training <training@hashicorp.com>
2017-08-14 14:08:39 +0000

    Initial commit

---
+++ main.tf
@@ -0,0 +1,60 @@
+#
+# DO NOT DELETE THESE LINES!
+#
+# Your AMI ID is:
+#
+#     ami-a142b2d8
+#
+# Your subnet ID is:
+#
+#     subnet-f49a11bd
+#
+# Your security group ID is:
+#
+#     sg-e46ef19c
+#
+# Your Identity is:
+#
+#     Idol-training-mouse
+#
+terraform {
+  backend "atlas" {
+    name = "garethlowe/training"
+  }
+}
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+variable "aws_region" {
+  default = "eu-west-1"
+}
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.aws_region}"
+}
+
+resource "aws_instance" "web" {
+  count                  = 2
+  ami                    = "ami-a142b2d8"
+  instance_type          = "t2.micro"
+  subnet_id              = "subnet-f49a11bd"
+  vpc_security_group_ids = ["sg-e46ef19c"]
+
+  tags {
+    "Identity" = "Idol-training-mouse"
+    "Name"     = "Gareth"
+    "Hello"    = "World"
+    "Index"    = "${count.index}"
+  }
+}
+
+output "public_dns" {
+  value = ["${aws_instance.web.*.public_dns}"]
+}
+
+output "public_ip" {
+  value = ["${aws_instance.web.*.public_ip}"]
+}
